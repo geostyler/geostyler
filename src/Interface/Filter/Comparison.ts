96fbf286a3bc9924c7911fdb7d8736f01df1e73d
Christian Mayer <chris@meggsimum.de>
2018-05-15 14:21:18 +0200

    Rm Models, Types, Interfaces covered by gs-style
    As we created our own declaration repository for the geostyler-style
    this removes all duplicated/unneeded Models, Types and Interfaces.

--- src/Interface/Filter/Comparison.ts
+++
@@ -1,6 +0,0 @@
-import Filter from './Filter';
-
-export default interface Comparison extends Filter {
-  1: string|number|boolean|null;
-  2: string|number|boolean|null;
-}
--- src/Interface/Filter/Filter.ts
+++
@@ -1,5 +0,0 @@
-import Operator from '../../Type/Operator';
-
-export default interface Filter extends Array<any> {
-  0: Operator;
-}
