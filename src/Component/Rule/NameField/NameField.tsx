2d30b9029eee52a11d9ab7fed20c65ae684adbab
Christian Mayer <chris@meggsimum.de>
2018-05-18 12:48:37 +0200

    Add a NameField component for a Rule

---
+++ src/Component/Rule/NameField/NameField.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Input } from 'antd';
+import FormItem from 'antd/lib/form/FormItem';
+
+// default props
+interface DefaultNameFieldProps {
+  label: string;
+  placeholder: string;
+}
+// non default props
+interface NameFieldProps extends Partial<DefaultNameFieldProps> {
+  onChange: ((newValue: string) => void);
+}
+
+/**
+ * Input field for a textual filter value.
+ */
+class NameField extends React.Component<NameFieldProps, any> {
+
+  public static defaultProps: DefaultNameFieldProps = {
+    label: 'Name',
+    placeholder: 'Enter Rule Name'
+  };
+
+  /**
+   * Extracts the text value of the ChangeEvent
+   * and passes it to the passed in 'onChange' handler.
+   */
+  valToString = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    this.props.onChange(value);
+  }
+
+  render() {
+
+    return (
+      <div className="gs-rule-namefield" style={{ }} >
+
+        <FormItem label={this.props.label} colon={false} >
+
+          <Input
+            style={{ width: '100%' }}
+            onChange={this.valToString}
+            placeholder={this.props.placeholder}
+          />
+
+        </FormItem>
+
+      </div>
+    );
+  }
+}
+
+export default NameField;
