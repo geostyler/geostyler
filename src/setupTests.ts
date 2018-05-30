955a20c610b8b428ca70a4b5b84a2905ead4994e
Kai Volland <kaivolland@yahoo.de>
2018-05-30 15:23:39 +0200

    Adds polyfill for createRange

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,28 @@
+/* tslint:disable */
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.Range = function Range() {};
+
+const createContextualFragment = (html) => {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  return div.children[0]; // so hokey it's not even funny
+};
+
+Range.prototype.createContextualFragment = (html) => createContextualFragment(html);
+
+// HACK: Polyfil that allows codemirror to render in a JSDOM env.
+global.window.document.createRange = function createRange() {
+  return {
+    setEnd: () => {},
+    setStart: () => {},
+    getBoundingClientRect: () => {
+      return { right: 0 };
+    },
+    getClientRects: () => [],
+    createContextualFragment,
+  };
+};
