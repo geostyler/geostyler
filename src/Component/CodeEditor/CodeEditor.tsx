2743febfbaadb58255347a9c3122fea80ea22782
Kai Volland <kaivolland@yahoo.de>
2018-05-30 14:13:51 +0200

    Adds SLD support to Editor

--- src/Component/CodeEditor/CodeEditor.tsx
+++ src/Component/CodeEditor/CodeEditor.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
+
+// Favourite Editor should be Monaco Editor but its React Wrapper is currently
+// not very stable
+import {
+  UnControlled as CodeMirror
+} from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
 
 import './CodeEditor.css';
 
@@ -8,14 +16,9 @@ import {
   StyleParserConstructable as GsStyleParserConstructable
 } from 'geostyler-style';
 
-// Favourite Editor should be Monaco Editor but its React Wrapper is currently
-// not very stable
 import {
-  UnControlled as CodeMirror
-} from 'react-codemirror2';
-
-require('codemirror/mode/xml/xml');
-require('codemirror/mode/javascript/javascript');
+  isEqual as _isEqual
+} from 'lodash';
 
 // default props
 interface DefaultCodeEditorProps {
@@ -24,14 +27,15 @@ interface DefaultCodeEditorProps {
 // non default props
 interface CodeEditorProps extends Partial<DefaultCodeEditorProps> {
   style?: GsStyle;
-  parsers: GsStyleParserConstructable[];
+  parser?: GsStyleParserConstructable;
   onStyleChange?: (rule: GsStyle) => void;
 }
 
 // state
 interface CodeEditorState {
-  style: GsStyle;
-  invalid: boolean;
+  value: string;
+  style?: GsStyle;
+  invalidMessage?: string;
 }
 
 /**
@@ -39,64 +43,111 @@ interface CodeEditorState {
  */
 class CodeEditor extends React.Component<CodeEditorProps, CodeEditorState> {
 
+  public static defaultProps: DefaultCodeEditorProps = {
+  };
+
   constructor(props: CodeEditorProps) {
     super(props);
     this.state = {
-      style: {
-        name: 'Circle',
-        type: 'Point',
-        rules: []
-      },
-      invalid: false
+      value: ''
     };
   }
 
-  static getDerivedStateFromProps(
-      nextProps: CodeEditorProps,
-      prevState: CodeEditorState): Partial<CodeEditorState> {
-    const newState: Partial<CodeEditorState> = {};
-    if (nextProps.style) {
-      newState.style = nextProps.style;
-    }
-    return newState;
+  static getDerivedStateFromProps(nextProps: CodeEditorProps) {
+    return {
+      style: nextProps.style
+    };
   }
 
-  public static defaultProps: DefaultCodeEditorProps = {
-  };
+  componentDidMount() {
+    if (this.props.style) {
+      this.updateValueFromStyle(this.props.style);
+    }
+  }
+
+  componentDidUpdate(prevProps: CodeEditorProps, prevState: CodeEditorState) {
+    if (this.props.style && !_isEqual(this.props.style, prevProps.style)) {
+      this.updateValueFromStyle(this.props.style);
+    }
+  }
+
+  updateValueFromStyle (style: GsStyle) {
+    this.valueFromStyleInput(style)
+      .then((parsedStyle: string) => {
+        this.setState({
+          value: parsedStyle
+        });
+      });
+  }
+
+  getModeByParser(): string {
+    if (this.props.parser && this.props.parser.name === 'SldStyleParser') {
+      return 'application/xml';
+    }
+    return 'application/json';
+  }
+
+  valueFromStyleInput(style: GsStyle) {
+    return new Promise((resolve, reject) => {
+      if (this.props.parser) {
+        const StyleParser = this.props.parser;
+        const parserInstance = new StyleParser();
+        resolve(parserInstance.writeStyle(style));
+      } else {
+        resolve(JSON.stringify(style, null, 2));
+      }
+    });
+  }
+
+  styleFromValue(value: string) {
+    return new Promise((resolve, reject) => {
+      if (this.props.parser) {
+        const StyleParser = this.props.parser;
+        const parserInstance = new StyleParser();
+        resolve(parserInstance.readStyle(value));
+      } else {
+        resolve(JSON.parse(value));
+      }
+    });
+  }
 
   /**
    *
    */
   onChange = (editor: any, data: any, value: string) => {
-    let style = this.state.style;
-    try {
-      style = JSON.parse(value);
-    } catch (error) {
-      // TODO improve this
-      this.setState({
-        invalid: true
-      });
-    }
-
+    let invalidMessage;
     this.setState({
-      style
+      value
     });
-    if (this.props.onStyleChange) {
-      this.props.onStyleChange(style);
+    try {
+      this.styleFromValue(value)
+        .then((style: GsStyle) => {
+          if (this.props.onStyleChange) {
+            this.props.onStyleChange(style);
+          }
+        }).catch(err => {debugger; });
+    } catch (error) {
+      invalidMessage = 'Error';
+    } finally {
+      this.setState({
+          invalidMessage
+      });
     }
   }
 
   render() {
-    const value = JSON.stringify(this.state.style, null, 2);
-
+    const value = this.state.value;
     return (
       <CodeMirror
         className="gs-code-editor"
         value={value}
         autoCursor={false}
         options={{
-          mode: 'application/json',
-          lineNumbers: true
+          gutters: ['CodeMirror-lint-markers'],
+          lint: true,
+          mode: this.getModeByParser(),
+          lineNumbers: true,
+          lineWrapping: true
         }}
         onChange={this.onChange}
       />
