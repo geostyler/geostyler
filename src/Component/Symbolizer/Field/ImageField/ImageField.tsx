4d4b3aa317cef7d8f58770ef74a726db6c8ea6d3
Kai Volland <kaivolland@yahoo.de>
2018-06-14 15:05:33 +0200

    Introduces Image-, Rotate- and SizeField

---
+++ src/Component/Symbolizer/Field/ImageField/ImageField.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import {
+  Input
+} from 'antd';
+
+// default props
+interface ImageFieldDefaultProps {
+  image: string;
+  label: string;
+  placeholder: string;
+}
+
+// non default props
+interface ImageFieldProps extends Partial<ImageFieldDefaultProps> {
+  onChange: ((image: string) => void);
+}
+
+/**
+ * ImageField
+ */
+class ImageField extends React.Component<ImageFieldProps, {}> {
+
+  public static defaultProps: ImageFieldDefaultProps = {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/67/OpenLayers_logo.svg',
+    label: 'Image',
+    placeholder: 'URL to image'
+  };
+
+  render() {
+    const {
+      image,
+      label,
+      placeholder,
+      onChange
+    } = this.props;
+
+    return (
+      <div className="editor-field image-field">
+        <span className="label">{`${label}:`}</span>
+        <Input
+          value={image}
+          placeholder={placeholder}
+          defaultValue={image}
+          onChange={(evt: any) => {
+            const value = evt.target.value;
+            onChange(value);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageField;
