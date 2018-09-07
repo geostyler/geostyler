abcc050ef8c1a3d8ba198920965bfeb593f4ca50
jansule <j_sule02@uni-muenster.de>
2018-09-07 14:45:29 +0200

    multiple fixes and improvements

--- src/index.js
+++ src/index.js
@@ -18,14 +18,22 @@ import FillEditor from './Component/Symbolizer/FillEditor/FillEditor';
 import TextEditor from './Component/Symbolizer/TextEditor/TextEditor';
 import IconEditor from './Component/Symbolizer/IconEditor/IconEditor';
 import GraphicEditor from './Component/Symbolizer/GraphicEditor/GraphicEditor';
+import MarkEditor from './Component/Symbolizer/MarkEditor/MarkEditor';
+import WellKnownNameEditor from './Component/Symbolizer/WellKnownNameEditor/WellKnownNameEditor';
 import ColorField from './Component/Symbolizer/Field/ColorField/ColorField';
+import ImageField from './Component/Symbolizer/Field/ImageField/ImageField';
+import KindField from './Component/Symbolizer/Field/KindField/KindField';
 import OffsetField from './Component/Symbolizer/Field/OffsetField/OffsetField';
 import OpacityField from './Component/Symbolizer/Field/OpacityField/OpacityField';
 import RadiusField from './Component/Symbolizer/Field/RadiusField/RadiusField';
+import RotateField from './Component/Symbolizer/Field/RotateField/RotateField';
+import SizeField from './Component/Symbolizer/Field/SizeField/SizeField';
+import WellKnownNameField from './Component/Symbolizer/Field/WellKnownNameField/WellKnownNameField';
 import WidthField from './Component/Symbolizer/Field/WidthField/WidthField';
 import GraphicTypeField from './Component/Symbolizer/Field/GraphicTypeField/GraphicTypeField';
 import LineCapField from './Component/Symbolizer/Field/LineCapField/LineCapField';
 import LineJoinField from './Component/Symbolizer/Field/LineJoinField/LineJoinField';
+import LineDashField from './Component/Symbolizer/Field/LineDashField/LineDashField';
 import UploadButton from './Component/UploadButton/UploadButton';
 import Style from './Component/Style/Style';
 import { localize } from './Component/LocaleWrapper/LocaleWrapper';
@@ -47,14 +55,22 @@ export {
   Preview,
   Editor,
   GraphicEditor,
+  MarkEditor,
+  WellKnownNameEditor,
   ColorField,
+  ImageField,
+  KindField,
   OffsetField,
   OpacityField,
   RadiusField,
+  RotateField,
+  SizeField,
+  WellKnownNameField,
   WidthField,
   GraphicTypeField,
   LineCapField,
   LineJoinField,
+  LineDashField,
   UploadButton,
   IconEditor,
   LineEditor,
