247d783c474193789f9da54a0e63d91c8d78d6d9
jansule <j_sule02@uni-muenster.de>
2018-10-02 10:19:10 +0200

    copy .vscode/tasks, lint fix, remove source-map, minify css, update gitignore and package.json

--- browser-build.config.js
+++ browser-build.config.js
@@ -1,51 +1,56 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
- module.exports = {
+module.exports = {
   entry: "./src/index.js",
   output: {
-      filename: "geostyler.js",
-      path: __dirname + "/browser",
-      library: 'GeoStyler'
+    filename: "geostyler.js",
+    path: __dirname + "/browser",
+    library: "GeoStyler"
   },
-   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-   resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
-   module: {
-      rules: [
-          {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: "css-loader"
-            })
-          },
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          {
-            test: /\.tsx?$/,
-            loader: "awesome-typescript-loader"
-          },
-          // locale js files need to be transpiled to es5
-          {
-            test: /\.js?$/,
-            include: /src\/locale/,
-            loader: 'babel-loader',
-            query: {
-              presets: ['@babel/env']
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                minimize: true
+              }
             }
-          }
-      ]
+          ]
+        })
+      },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+      // locale js files need to be transpiled to es5
+      {
+        test: /\.js?$/,
+        include: /src\/locale/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/env"]
+        }
+      }
+    ]
   },
-   plugins: [
+  plugins: [
     new ExtractTextPlugin("geostyler.css")
   ],
-   // When importing a module whose path matches one of the following, just
+  // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
