a6ed68c8ed8c15f09e51c4ef7d97353b65d45a1a
jansule <j_sule02@uni-muenster.de>
2018-10-29 14:44:10 +0100

    fix preview spec

--- src/Component/Symbolizer/Preview/Preview.spec.tsx
+++ src/Component/Symbolizer/Preview/Preview.spec.tsx
@@ -1,13 +1,22 @@
 import { Preview } from './Preview';
 import TestUtil from '../../../Util/TestUtil';
 import en_US from '../../../locale/en_US';
+import { Symbolizer } from 'geostyler-style';
 
 describe('Preview', () => {
 
   let wrapper: any;
+  const dummySymbolizers: Symbolizer[] = [{
+    kind: 'Mark',
+    wellKnownName: 'Circle',
+    color: '#FF0000'
+  }];
+
   beforeEach(() => {
     wrapper = TestUtil.shallowRenderComponent(Preview, {
-      locale: en_US.GsPreview
+      locale: en_US.GsPreview,
+      onSymbolizerChange: jest.fn(),
+      symbolizers: dummySymbolizers
     });
   });
 
