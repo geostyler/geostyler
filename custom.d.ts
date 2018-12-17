2a0cae242fd5201ce3db536e740d574a887fe641
arieger <rieger@terrestris.de>
2018-12-17 11:20:13 +0100

    introduce custom.d.ts to provide missing ts declarations for ol5 classes

---
+++ custom.d.ts
@@ -0,0 +1,13 @@
+declare module 'ol/format/GeoJSON';
+declare module 'ol/layer/Tile';
+declare module 'ol/layer/Vector';
+declare module 'ol/source/OSM';
+declare module 'ol/source/Vector';
+// will be declared in geostyler-openlayers-parser
+// declare module 'ol/Feature';
+declare module 'ol/Map';
+declare module 'ol/View';
+declare module 'ol/geom/LineString';
+// will be declared in geostyler-openlayers-parser
+// declare module 'ol/geom/Point';
+declare module 'ol/geom/Polygon';
