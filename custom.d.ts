99f36305cfa364a109d83328f151be8cec51cb87
arieger <rieger@terrestris.de>
2019-01-07 13:39:43 +0100

    remove redundant module declarations for ol/geom/Point and ol/Feature classes

--- custom.d.ts
+++ custom.d.ts
@@ -3,11 +3,7 @@ declare module 'ol/layer/Tile';
 declare module 'ol/layer/Vector';
 declare module 'ol/source/OSM';
 declare module 'ol/source/Vector';
-// will be declared in geostyler-openlayers-parser
-// declare module 'ol/Feature';
 declare module 'ol/Map';
 declare module 'ol/View';
 declare module 'ol/geom/LineString';
-// will be declared in geostyler-openlayers-parser
-// declare module 'ol/geom/Point';
 declare module 'ol/geom/Polygon';
