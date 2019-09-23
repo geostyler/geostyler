949b09867909e49131112b4c0c8578fb635f3465
jansule <j_sule02@uni-muenster.de>
2019-09-23 14:32:59 +0200

    customise TextEditor

--- src/Component/CompositionContext/CompositionContext.tsx
+++ src/Component/CompositionContext/CompositionContext.tsx
@@ -25,7 +25,19 @@ export interface Compositions {
     outlineWidthField?: React.ReactElement | false;
     outlineDasharrayField?: React.ReactElement | false;
     graphicEditorField?: React.ReactElement | false;
-  }
+  };
+  TextEditor?: {
+    templateField?: React.ReactElement | false;
+    colorField?: React.ReactElement | false;
+    fontField?: React.ReactElement | false;
+    opacityField?: React.ReactElement | false;
+    sizeField?: React.ReactElement | false;
+    offsetXField?: React.ReactElement | false;
+    offsetYField?: React.ReactElement | false;
+    rotateField?: React.ReactElement | false;
+    haloColorField?: React.ReactElement | false;
+    haloWidthField?: React.ReactElement | false;
+  };
 }
 
 export const CompositionContext = React.createContext({});
