6558414db3310ff3df4254a6f422e2c747da8c2c
Kai Volland <kaivolland@yahoo.de>
2021-04-16 13:59:22 +0200

    adds react-testing-library

--- jest/setup.js
+++ jest/setup.js
@@ -33,3 +33,17 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
