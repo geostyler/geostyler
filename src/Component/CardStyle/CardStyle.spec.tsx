bda1a91e9c084665a05be3f5ca7409188987b1fa
jansule <suleiman@terrestris.de>
2022-06-09 15:35:29 +0200

    add tests for CardStyle component

--- src/Component/CardStyle/CardStyle.spec.tsx
+++ src/Component/CardStyle/CardStyle.spec.tsx
@@ -27,10 +27,34 @@
  */
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import { CardStyle } from './CardStyle';
+import { IconSymbolizer, Rule, Style, Symbolizer } from 'geostyler-style';
+import SymbolizerUtil from '../../Util/SymbolizerUtil';
+import { IconLibrary } from '../Symbolizer/IconSelector/IconSelector';
 
 describe('CardStyle', () => {
+
+  let onStyleChangeDummy: jest.Mock;
+  let dummyStyle: Style;
+  let dummyRule: Rule;
+  let dummySymbolizer: Symbolizer;
+
+  beforeEach(() => {
+    onStyleChangeDummy = jest.fn();
+
+    dummyStyle = {
+      name: 'foo',
+      rules: []
+    };
+
+    dummyRule = {
+      name: 'foo',
+      symbolizers: []
+    };
+
+    dummySymbolizer = SymbolizerUtil.generateSymbolizer('Icon');
+  });
 
   it('is defined', () => {
     expect(CardStyle).toBeDefined();
@@ -39,6 +63,196 @@ describe('CardStyle', () => {
   it('renders correctly', () => {
     const cardStyle = render(<CardStyle />);
     expect(cardStyle.container).toBeInTheDocument();
+  });
+
+  describe('onStyleChange', () => {
+
+    it('calls a passed onStyleChange function', async () => {
+      const cardStyle = render(<CardStyle onStyleChange={onStyleChangeDummy} />);
+      const input = cardStyle.container.querySelector('.gs-namefield');
+      await act(async () => {
+        fireEvent.change(input, {
+          target: {
+            value: 'foo'
+          }
+        });
+      });
+
+      expect(onStyleChangeDummy).toHaveBeenCalled();
+    });
+
+    it('calls a passed onStyleChange function with the changed style as argument', async () => {
+      const name = 'bar';
+      const cardStyle = render(<CardStyle style={dummyStyle} onStyleChange={onStyleChangeDummy} />);
+      const input = cardStyle.container.querySelector('.gs-namefield');
+      await act(async () => {
+        fireEvent.change(input, {
+          target: {
+            value: name
+          }
+        });
+      });
+
+      dummyStyle.name = name;
+
+      expect(onStyleChangeDummy).toHaveBeenCalledWith(dummyStyle);
+    });
+  });
+
+  describe('StyleOverview', () => {
+
+    it('is shown by default', () => {
+      const cardStyle = render(<CardStyle />);
+      const styleOverview = cardStyle.container.querySelector('.gs-style-overview');
+      expect(styleOverview).not.toBeNull();
+    });
+
+    it('switches to RuleOverview when clicking on a rule', async () => {
+      dummyStyle.rules = [dummyRule];
+      const cardStyle = render(<CardStyle style={dummyStyle} />);
+      const ruleCard = cardStyle.container.querySelector('.gs-rule-card');
+
+      await act(async () => {
+        fireEvent.click(ruleCard);
+      });
+
+      const ruleOverview = cardStyle.container.querySelector('.gs-rule-overview');
+      expect(ruleOverview).not.toBeNull();
+    });
+
+    it('switches to the RuleGenerator when clicking on the classification button', async () => {
+      const cardStyle = render(<CardStyle />);
+
+      const classificationButton = cardStyle.container.querySelector('.gs-classification-button');
+      await act(async () => {
+        fireEvent.click(classificationButton);
+      });
+
+      const ruleGenerator = cardStyle.container.querySelector('.gs-rule-generator');
+      expect(ruleGenerator).not.toBeNull();
+    });
+
+    it('switches to the BulkEditor when clicking on the multi-edit button', async () => {
+      const cardStyle = render(<CardStyle />);
+
+      const multiSelectToggle = cardStyle.container.querySelector('.gs-multi-select-toggle');
+      await act(async () => {
+        fireEvent.click(multiSelectToggle);
+      });
+
+      const multiEditButton = cardStyle.container.querySelector('.gs-edit-rules-button');
+      await act(async () => {
+        fireEvent.click(multiEditButton);
+      });
+
+      const ruleGenerator = cardStyle.container.querySelector('.gs-bulkeditor');
+      expect(ruleGenerator).not.toBeNull();
+    });
+  });
+
+  describe('RuleOverview', () => {
+
+    it('switches to SymbolizerView when clickin on a symbolizer', async () => {
+      dummyRule.symbolizers = [dummySymbolizer];
+      dummyStyle.rules = [dummyRule];
+      const cardStyle = render(<CardStyle style={dummyStyle} />);
+
+      const ruleCard = cardStyle.container.querySelector('.gs-rule-card');
+      await act(async () => {
+        fireEvent.click(ruleCard);
+      });
+
+      const symbolizerCard = cardStyle.container.querySelector('.gs-symbolizer-card');
+      await act(async () => {
+        fireEvent.click(symbolizerCard);
+      });
+
+      const editorView = cardStyle.container.querySelector('.gs-symbolizer-editor');
+      expect(editorView).not.toBeNull();
+    });
+
+    it('switches to the FilterTree when clicking on the add filter button', async () => {
+      dummyRule.symbolizers = [dummySymbolizer];
+      dummyStyle.rules = [dummyRule];
+      const cardStyle = render(<CardStyle style={dummyStyle} />);
+
+      const ruleCard = cardStyle.container.querySelector('.gs-rule-card');
+      await act(async () => {
+        fireEvent.click(ruleCard);
+      });
+
+      const addFilterButton = cardStyle.container.querySelector('.gs-filter-overview-add button');
+      await act(async () => {
+        fireEvent.click(addFilterButton);
+      });
+
+      const filterTree = cardStyle.container.querySelector('.gs-filter-tree');
+      expect(filterTree).not.toBeNull();
+    });
+  });
+
+  // describe('RuleGenerator', () => {
+
+  //   TODO make this test work
+  //   it('switches to the StyleOverview when clicking on classify button', async () => {
+  //     const cardStyle = render(<CardStyle />);
+
+  //     const classificationButton = cardStyle.container.querySelector('.gs-classification-button');
+  //     await act(async () => {
+  //       fireEvent.click(classificationButton);
+  //     });
+
+  //     const attributeInput = await cardStyle.findByPlaceholderText('Select Attribute');
+  //     await act(async () => {
+  //       fireEvent.change(attributeInput, {
+  //         target: {
+  //           value: 'foo'
+  //         }
+  //       });
+  //     });
+
+  //     const classifyButton = cardStyle.container.querySelector('.gs-rule-generator-submit-button');
+  //     await act(async () => {
+  //       fireEvent.click(classifyButton);
+  //     });
+
+  //     const styleOverview = cardStyle.container.querySelector('.gs-style-overview');
+  //     expect(styleOverview).not.toBeNull();
+  //   });
+  // });
+
+  describe('Editor', () => {
+
+    it('switches to the IconSelector when clicking on the icon library button', async () => {
+      dummyRule.symbolizers = [dummySymbolizer];
+      dummyStyle.rules = [dummyRule];
+      const iconLibraries: IconLibrary[] = [{
+        name: 'foo',
+        icons: [{
+          caption: 'bar',
+          src: 'foo.bar'
+        }]
+      }];
+      const cardStyle = render(<CardStyle style={dummyStyle} iconLibraries={iconLibraries} />);
+
+      const ruleCard = cardStyle.container.querySelector('.gs-rule-card');
+      await act(async () => {
+        fireEvent.click(ruleCard);
+      });
+
+      const symbolizerCard = cardStyle.container.querySelector('.gs-symbolizer-card');
+      await act(async () => {
+        fireEvent.click(symbolizerCard);
+      });
+
+      const galleryIcon = cardStyle.container.querySelector('.gs-image-field-gallery-icon');
+      await act(async () => {
+        fireEvent.click(galleryIcon);
+      });
+
+      const iconSelector = cardStyle.container.querySelector('.gs-icon-selector');
+      expect(iconSelector).not.toBeNull();
+    });
   });
 
 });
