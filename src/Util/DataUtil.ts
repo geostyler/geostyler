4d9c7c394e7856efcada6ad73fd42d5217af349d
Kai Volland <volland@terrestris.de>
2022-06-21 09:53:18 +0200

    transform RuleTable to React.FC

--- src/Util/DataUtil.ts
+++ src/Util/DataUtil.ts
@@ -36,14 +36,14 @@ class DataUtil {
    * Checks if Data object is of type VectorData.
    */
   static isVector = (v: Data): v is VectorData => {
-    return (<VectorData> v).exampleFeatures !== undefined;
+    return (<VectorData> v)?.exampleFeatures !== undefined;
   };
 
   /**
    * Checks if Data object is of type RasterData.
    */
   static isRaster = (r: Data): r is RasterData => {
-    return (<RasterData> r).rasterBandInfo !== undefined;
+    return (<RasterData> r)?.rasterBandInfo !== undefined;
   };
 }
 
